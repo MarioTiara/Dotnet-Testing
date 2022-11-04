d351695621748831bbb2260109694e4fe710400f
XTR\mario.pratama <mario.pratama@xtremax.com>
2022-11-04 08:36:51 +0700

    [CalculatorTest.cs] Add FiboNumbers

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace Dotnet_UnitTest
 {
     public class Calculator
     {
+        public List<int> FiboNumbers => new List<int>{1,2,2,3,5,8,13};
         public int Add (int x, int y){
             return x+y;
         }
