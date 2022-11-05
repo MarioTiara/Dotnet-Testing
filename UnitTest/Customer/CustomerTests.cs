427ed8f703d984295ae9efbdfffe34b4c131734d
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-05 15:25:19 +0700

    [CustomerTests] Apply CustomerFixture

--- UnitTest/Customer/CustomerTests.cs
+++ UnitTest/Customer/CustomerTests.cs
@@ -1,36 +1,43 @@
 using Xunit;
 using System;
 using Dotnet_UnitTest.Models;
+using Dotnet_UnitTest.UnitTest.customer;
+
 namespace Dotnet_UnitTest
 {
-    public class CustomerTests
+    [Collection("Customer")]
+    public class CustomerTests:IClassFixture<CustomerFixture>
     {
-        Customer customer => new Customer();
+        private readonly  CustomerFixture  _customerFixture;
+        public CustomerTests(CustomerFixture  customerFixture){
+            _customerFixture=customerFixture;
+        }
+
         [Fact]
         public void CheckNameNotEmpty(){
-            Assert.NotNull(customer.Name);
-            Assert.False(string.IsNullOrEmpty(customer.Name));
+            Assert.NotNull(_customerFixture.customer.Name);
+            Assert.False(string.IsNullOrEmpty(_customerFixture.customer.Name));
         }
         [Fact]
         public void CheckAgeIsInrange(){
-            Assert.InRange(customer.Age, 25,40);
+            Assert.InRange(_customerFixture.customer.Age, 25,40);
         }
 
         [Fact]
         public void CheckAgeIsNotInrange(){
-            Assert.NotInRange(customer.Age, 40,50);
+            Assert.NotInRange(_customerFixture.customer.Age, 40,50);
         }
         
         [Fact]
         public void GetOrdersBynNameNotNull(){
-           var exceptionDetails= Assert.Throws<ArgumentException>(()=>customer.GetOrdersByName(null));
+           var exceptionDetails= Assert.Throws<ArgumentException>(()=>_customerFixture.customer.GetOrdersByName(null));
            Assert.Equal("String is null or empty", exceptionDetails.Message);
         }
 
         [Fact]
         public void LoyalCustomerForOrdersG100(){
             var customer =CustomerFactory.CreateCustomerInstance(102);
-            var loyalCustomer=Assert.IsType<LoyalCustomer>(customer);
+            var loyalCustomer=Assert.IsType<LoyalCustomer>(_customerFixture.customer);
             Assert.Equal(10, loyalCustomer.Discount);
         }
     }
