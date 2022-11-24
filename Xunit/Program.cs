76d1fee08e0a550a339cbde3304972aa888cc607
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-24 20:15:13 +0700

    [UnitTest] Syncronize project folder and add new porject for mocking Unit test

---
+++ Xunit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dotnet_UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool val;
+            string input;
+
+            input = bool.TrueString;
+            val = bool.Parse(input);
+            Console.WriteLine("'{0}' parsed as {1}", input, val);
+        }
+
+    }
+}
