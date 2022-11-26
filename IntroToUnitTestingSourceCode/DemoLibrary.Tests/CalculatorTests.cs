551fb2fe1efc28306490feb57c4915572efe3176
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-26 10:44:06 +0700

    [DemoLibrary.Tests] Add Theori Data on UNitTest

---
+++ IntroToUnitTestingSourceCode/DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(4,3,7)]
+        [InlineData(21,5.25,26.25)]
+        [InlineData(double.MaxValue,5, double.MaxValue)]
+        public void Add_SimpleValueShouldCalculated(double x, double y, double expected)
+        {
+            //Arrange
+            
+            //Act
+            double actual = Calculator.Add(x,y);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8,4,2)]
+        public void Divide_SimpleValueShouldCalculatd(double x, double y, double expected)
+        {
+            //arrange
+            double actual = Calculator.Divide(x, y);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
