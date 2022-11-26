077ee573d1d8692a0da9b01b0856bf69beff2a5d
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-26 13:46:51 +0700

    [DemoLibrary.Tests] DataAccessTests=>AddPersonToPeopleList Test

---
+++ IntroToUnitTestingSourceCode/DemoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel newPerson = new PersonModel { FirstName = "Mario", LastName = "Tiara" };
+            List<PersonModel> people= new List<PersonModel>();
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+            Assert.True(people.Count == 1);
+            Assert.Contains<PersonModel>(newPerson, people);
+        }
+
+        [Theory]
+        [InlineData("Mario", "", "LastName")]
+        [InlineData("", "Pratama", "FirstName")]
+        public void AddPersonToPeopleList_ShouldFail(string firstName, string lastName, string param)
+        {
+            PersonModel newPerson = new PersonModel { FirstName = firstName, LastName = lastName };
+            List<PersonModel> people = new List<PersonModel>();
+            Assert.Throws<ArgumentException>(param, () => DataAccess.AddPersonToPeopleList(people, newPerson));
+        }
+    }
+}
